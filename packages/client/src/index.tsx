1887dad89f4fe4417e9aa0cf7636577a2e07b1cd
neilchetna <neelchetna18@gmail.com>
2023-03-26 18:24:17 +0530

    :wrench: Added typescript and prettier config to the client

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import NewApp from "./app"
 
 const App = () => {
   return (
     <div>
       <h1>Hello!!</h1>
       <h2>Welcome to your First React App..!</h2>
+      <NewApp num={19}  />
     </div>
   )
 }
--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,31 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-   entry: './src/index.js',
-   output: {
-      path: path.join(__dirname, '/dist'),
-      filename: 'bundle.js'
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 8080
+  },
+  resolve:
+   {
+     extensions: ['.tsx', '.ts', '.js']
    },
-   devServer: {
-      port: 8080
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-         },
-         {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        }
-      ]
-   },
-   plugins:[
-       new HtmlWebpackPlugin({
-            template: path.join(__dirname,'/src/index.html')
-       }) 
-   ]
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.html')
+    })
+  ]
 }
